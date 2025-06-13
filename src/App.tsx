6541bab4595d7c8dfc672340007090271e2a0331
nomativ <toedgar@pm.me>
2025-06-13 14:44:30 -0700

    copy improvements

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,10 @@ function App() {
           <div className="w-[470px] p-3 bg-[#173253] rounded-[30px] flex-col justify-start items-start gap-4 inline-flex self-center">
             <div className="p-8 bg-[#d7e4f3] rounded-3xl border border-black flex-col justify-start items-start gap-6 flex overflow-hidden">
               <div className="h-[85px] flex-col justify-start items-start flex">
-                <div className="text-black/50 text-2xl">Software and web design leader</div>
-                <div className="text-black/90 text-[40px] font-medium font-['Bricolage Grotesque']">10 years experience</div>
+                <div className="text-black/50 text-2xl">Product and web designer</div>
+                <div className="text-black/90 text-[40px] font-medium font-['Bricolage Grotesque']">10 years expertise</div>
               </div>
-              <div className="text-black text-base font-medium">I'm a designer based in Oregon with 10+ years of experience in designing and leading product design initiatives.</div>
+              <div className="text-black text-base font-medium">I turn complex challenges into thoughtfully crafted, user-led solutions that feel natural, useful and accessible to everyone.</div>
               <div className="flex flex-row items-center gap-4">
                 <a href="https://edgar-p.notion.site/" className="px-4 py-2 bg-gradient-to-b from-[#0f1825] to-[#0f1825] rounded-[999px] border border-white justify-center items-center gap-1 inline-flex">
                   <span className="text-center text-[#cccccc] text-base font-medium leading-loose">Portfolio</span>
