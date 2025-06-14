67ad12a870216e0bc8b480fdcda26722bb4bfde2
nomativ <toedgar@pm.me>
2025-06-13 21:59:57 -0700

    general improvements

--- src/App.tsx
+++ src/App.tsx
@@ -20,15 +20,15 @@ function App() {
         <div className="max-w-[900px] flex md:flex-row flex-col md:gap-18 gap-8 mx-auto items-center">
           <div className="flex flex-col items-center gap-6">
             <img src={profilePic} alt='Edgar Priday profile picture' className="w-67 h-67 rounded-[30px]" />
-            <span className="md:text-[3.2em] text-[2.5em] text-white min-w-fit">Edgar Priday</span>
+            <span className="text-[40px] text-white min-w-fit">Edgar Priday</span>
           </div>
-          <div className="md:w-[470px] md:mx-0 mx-6 p-3 bg-[#173253] rounded-[30px] flex-col justify-start items-start gap-4 inline-flex self-center">
+          <div className="md:w-[470px] md:mx-0 mx-3 p-3 bg-[#173253] rounded-[30px] flex-col justify-start items-start gap-4 inline-flex self-center">
             <div className="p-8 bg-[#d7e4f3] rounded-3xl border border-black flex-col justify-start items-start gap-6 flex overflow-hidden">
-              <div className="h-[85px] flex-col justify-start items-start flex">
-                <div className="text-black/50 text-2xl">Product and web designer</div>
-                <div className="text-black/90 md:text-[40px] text-[32px] font-medium">10 years expertise</div>
+              <div className="flex-col justify-start items-start flex">
+                <div className="text-black/50 text-2xl">Multidisciplinary</div>
+                <div className="text-black/90 md:text-[40px] text-[32px] font-medium leading-[40px]">Product designer</div>
               </div>
-              <div className="text-black text-base font-medium">I turn complex challenges into thoughtfully crafted, user-led solutions that feel natural, useful and accessible to everyone.</div>
+              <div className="text-black text-base font-medium">For the last <b>10 years</b>, I've turned complex challenges into thoughtfully crafted, user-led solutions that feel natural, useful and accessible to everyone.</div>
               <div className="flex flex-row items-center gap-4">
                 <a href="https://edgar-p.notion.site/" className="px-4 py-2 bg-gradient-to-b from-[#0f1825] to-[#0f1825] rounded-[999px] border border-white justify-center items-center gap-1 inline-flex">
                   <span className="text-center text-[#cccccc] text-base font-medium leading-loose">Portfolio</span>
@@ -39,8 +39,8 @@ function App() {
               < TextCarousel slides={heroTexts} />
             </div>
             <div className="w-full inline-flex px-2 pb-1 justify-between">
-              <div className="text-white text-base">Time zone: PST (UTC-7)</div>
-              <div className="text-white text-base flex items-center gap-2">
+              <div className="text-white md:text-base text-sm">Time zone: PST (UTC-7)</div>
+              <div className="text-white md:text-base text-sm flex items-center gap-2">
                 <div className="w-3 h-3 rounded-full bg-green-500"></div>
                 Available for work
               </div>
--- src/components/MtnBackground.tsx
+++ src/components/MtnBackground.tsx
@@ -2,13 +2,14 @@ import bgImage from '../assets/bg-image.png'
 
 export default function MtnBackground() {
     return (
-        <>
-            <div>
-                <img className="absolute -z-10 w-screen top-0 opacity-90" src={bgImage} />
-                <div className="absolute -z-10 w-[1459.37px] h-[606.24px] origin-top-left rotate-[0.91deg] opacity-75 mix-blend-hard-light bg-[radial-gradient(ellipse_82.87%_98.01%_at_97.00%_98.09%,_rgba(102,_216,_255,_0.20)_0%,_rgba(102,_216,_255,_0)_100%)] rounded-full blur-[100px]">
-                </div>
-                <div className="absolute -z-10 w-80 h-36 origin-top-left rotate-[0.91deg] opacity-100 mix-blend-hard-light bg-[radial-gradient(ellipse_67.05%_179.10%_at_16.58%_39.07%,_rgba(14,_26,_45,_0.15)_0%,_rgba(14,_26,_45,_0)_100%)] rounded-full blur-[100px]"></div>
-            </div>
-        </>
+        <div className="absolute top-0 left-0 -z-10 w-full overflow-visible">
+            <img
+                className="w-full opacity-90"
+                src={bgImage}
+                alt="Mountain background"
+            />
+            <div className="absolute top-0 left-0 w-full h-full origin-top-left rotate-[0.91deg] opacity-75 mix-blend-hard-light bg-[radial-gradient(ellipse_82.87%_98.01%_at_97.00%_98.09%,_rgba(102,_216,_255,_0.20)_0%,_rgba(102,_216,_255,_0)_100%)] rounded-full blur-[100px]" />
+            <div className="absolute top-0 left-0 w-80 h-36 origin-top-left rotate-[0.91deg] opacity-100 mix-blend-hard-light bg-[radial-gradient(ellipse_67.05%_179.10%_at_16.58%_39.07%,_rgba(14,_26,_45,_0.15)_0%,_rgba(14,_26,_45,_0)_100%)] rounded-full blur-[100px]" />
+        </div>
     )
 }
